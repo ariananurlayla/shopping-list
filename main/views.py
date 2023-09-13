e6b7706c68555204f4a4ff27d1999e9cd8e804a7
ariana <ariana.nurlayla@ui.ac.id>
2023-09-13 15:46:12 +0700

    add: import, create_product, update show_main

--- main/views.py
+++ main/views.py
@@ -1,10 +1,26 @@
+from django.http import HttpResponseRedirect
+from main.forms import ProductForm
+from django.urls import reverse
 from django.shortcuts import render
 
 # Create your views here.
 def show_main(request):
+    products = Product.objects.all()
+
     context = {
         'name': 'Ariana Nurlayla Syabandini',
-        'class': 'PBP C'
+        'class': 'PBP C',
+        'products': products
     }
 
     return render(request, "main.html", context)
+
+def create_product(request):
+    form = ProductForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form}
+    return render(request, "create_product.html", context)
