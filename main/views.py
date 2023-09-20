1af8f6896ce39c03ceaa97460d9da14e027ba283
ariana <ariana.nurlayla@ui.ac.id>
2023-09-20 15:22:27 +0700

    membuat fungsi login

--- main/views.py
+++ main/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render
 from main.models import Product
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def show_main(request):
@@ -59,3 +60,15 @@ def register(request):
     context = {'form':form}
     return render(request, 'register.html', context)
 
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('main:show_main')
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {}
+    return render(request, 'login.html', context)
