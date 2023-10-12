35c2400e24a165f348624417743fe2bd0b2e375f
ariana <ariana.nurlayla@ui.ac.id>
2023-10-12 20:17:15 +0700

    Tutorial 5 fixed

--- main/views.py
+++ main/views.py
@@ -1,24 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponseNotFound
-from main.forms import ProductForm
+import datetime
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseNotFound
+from main.forms import ProductForm, Product
 from django.urls import reverse
-from main.models import Product
-from django.http import HttpResponse
 from django.core import serializers
-from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages  
-from django.contrib.auth import authenticate, login
-from django.contrib.auth import logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
-import datetime
-from django.http import HttpResponseRedirect
-from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 @login_required(login_url='/login')
-
 def show_main(request):
     products = Product.objects.filter(user=request.user)
 
@@ -39,7 +32,7 @@ def create_product(request):
         product.user = request.user
         product.save()
         return HttpResponseRedirect(reverse('main:show_main'))
-
+    
     context = {'form': form}
     return render(request, "create_product.html", context)
 
@@ -78,7 +71,7 @@ def login_user(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
+            response = HttpResponseRedirect(reverse("main:show_main")) 
             response.set_cookie('last_login', str(datetime.datetime.now()))
             return response
         else:
@@ -116,8 +109,7 @@ def delete_product(request, id):
     return HttpResponseRedirect(reverse('main:show_main'))
 
 def get_product_json(request):
-    # sort by id
-    product_item = Product.objects.filter(user=request.user) 
+    product_item = Product.objects.all()
     return HttpResponse(serializers.serialize('json', product_item))
 
 @csrf_exempt
